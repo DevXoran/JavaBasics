cd1961ae62c58e6454110b590a2720e74df105a7
wegerer <wegerer@192.168.228.21>
2015-11-24 13:16:14 +0100

    initial commit

---
+++ src/datatypes2/Autoboxing.java
@@ -0,0 +1,21 @@
+package datatypes2;
+
+public class Autoboxing {
+
+    private static int cube(int x) {
+        
+        return x * x * x;                
+    }
+    
+    public static void main(String[] args) {
+        
+        Integer i = 10;
+        int j = i;
+        
+        System.out.println(i);
+        System.out.println(j);
+        
+        Integer a = cube(i);
+        System.out.println(a);
+    }
+}
